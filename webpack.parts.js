c864517065768735e370cf4f9bd00b5c51c5f724
Jelle Rienstra <jelle@fashionunited.com>
2018-08-17 14:18:56 +0200

    WIP webpack

--- webpack.parts.js
+++ webpack.parts.js
@@ -1,3 +1,6 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurifyCSSPlugin = require("purifycss-webpack");
+
 exports.devServer = ({ host, port } = {}) => ({
   devServer: {
     stats: "errors-only",
@@ -5,5 +8,54 @@ exports.devServer = ({ host, port } = {}) => ({
     port, // Defaults to 8080
     open: true,
     overlay: true,
+  },
+});
+
+exports.loadCSS = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        include,
+        exclude,
+
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+});
+
+exports.extractCSS = ({ include, exclude, use = [] }) => {
+  // Output extracted CSS to a file
+  const plugin = new MiniCssExtractPlugin({
+    filename: "[name].css",
+  });
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          include,
+          exclude,
+
+          use: [
+            MiniCssExtractPlugin.loader,
+          ].concat(use),
+        },
+      ],
+    },
+    plugins: [plugin],
+  };
+};
+
+exports.purifyCSS = ({ paths }) => ({
+  plugins: [new PurifyCSSPlugin({ paths })],
+});
+
+exports.autoprefix = () => ({
+  loader: "postcss-loader",
+  options: {
+    plugins: () => [require("autoprefixer")()],
   },
 });
